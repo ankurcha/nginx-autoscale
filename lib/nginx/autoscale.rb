932c441c2651ad60c8de903389236c0287252733
Ankur Chauhan <achauhan@brightcove.com>
2013-10-25 13:54:24 -0700

    Add more logging

--- lib/nginx/autoscale.rb
+++ lib/nginx/autoscale.rb
@@ -35,24 +35,12 @@ module Nginx
           `tar -xzvf nginx-1.2.6.tar.gz`
 
           Dir.chdir('/tmp/nginx-1.2.6') do
-            module_commands = ['--without-http_scgi_module',
-                               '--without-http_uwsgi_module',
-                               '--without-http_fastcgi_module',
-                               '--without-http_empty_gif_module',
-                               '--without-http_ssi_module',
-                               '--without-http_geo_module',
-                               '--without-mail_pop3_module',
-                               '--without-mail_imap_module',
-                               '--without-mail_smtp_module',
-                               '--with-http_degradation_module',
-                               '--with-http_ssl_module']
-
             # compile and install nginx to /opt/nginx
             puts 'configure nginx build'
-            `./configure --prefix=/opt/nginx --user=nginx #{module_commands.join(' ')}`
+            `./configure --prefix=/opt/nginx --user=nginx --without-http_scgi_module --without-http_uwsgi_module --without-http_fastcgi_module --without-http_empty_gif_module --without-http_ssi_module --without-http_geo_module --without-mail_pop3_module --without-mail_imap_module --without-mail_smtp_module --with-http_degradation_module --with-http_ssl_module 2>&amp;1`
 
             puts 'make and install nginx'
-            `make && make install`
+            `make && make install 2>&amp;1`
 
             puts 'configure nginx'
             `mkdir -p /opt/nginx/conf/conf.d /opt/nginx/conf/sites-available /opt/nginx/conf/sites-enabled /opt/nginx/conf/certs`
